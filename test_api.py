19519132c6098b5e79a3962c596b3deaacc2eeae
DinglyCoder <akaman.boss@gmail.com>
2025-10-01 20:28:19 -0500

    added pagination

--- test_api.py
+++ test_api.py
@@ -5,10 +5,19 @@ from dotenv import load_dotenv
 import os
 
 import base64
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 
 load_dotenv()
+
+base_url = "https://api.elections.kalshi.com"
+public_key = os.getenv("KALSHI_PUBLIC_KEY")
+private_key_path = os.getenv("KALSHI_PRIVATE_KEY_PATH")
+
+
+def load_private_key(path):
+    with open(path, "rb") as f:
+        return serialization.load_pem_private_key(f.read(),password=None)
 
 def sign_request(private_key, timestamp, method, path):
     # Create the message to sign
@@ -27,23 +36,25 @@ def sign_request(private_key, timestamp, method, path):
     # Return base64 encoded
     return base64.b64encode(signature).decode('utf-8')
 
+def get(url, headers, params=None):
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
 # get all events from Kalshi API
-def get_all_events():
-
+def get_all_events(params=None):
+    all_events = []
+    cursor = None
     endpoint_path = "/trade-api/v2/events"
-
-    url = f"https://api.elections.kalshi.com{endpoint_path}"
-    response = requests.get(url)
+    url = f"{base_url}{endpoint_path}"
 
     # Get the current timestamp
     current_timestamp = int(time.time() * 1000)
 
-    public_key = os.getenv("KALSHI_PUBLIC_KEY")
-    private_key = os.getenv("KALSHI_PRIVATE_KEY")
-
-    print("Public Key:", public_key)
-    print("Private Key:", private_key)
-
+    private_key = load_private_key(private_key_path)
     signature = sign_request(private_key, current_timestamp, "GET", endpoint_path)
 
     headers = {
@@ -52,16 +63,31 @@ def get_all_events():
         "KALSHI-ACCESS-SIGNATURE": signature
     }
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+    while True:
+        print(f"Fetching events with cursor: {cursor}")
+        if cursor:
+            params = {
+                'limit': 200,
+            }
+            params['cursor'] = cursor
+
+        response = get(url, headers, params)
+
+        if response and 'events' in response:
+            all_events.extend(response['events'])
+            cursor = response.get('cursor')
+            if not cursor:
+                break
+        else:
+            break
+
+    return all_events
     
 
 if __name__ == "__main__":
     events = get_all_events()
-    if events:
-        print(json.dumps(events, indent=4))
-    else:
-        print("No events found.")
+    print(f"Total events fetched: {len(events)}")
+
+    
+
+    
